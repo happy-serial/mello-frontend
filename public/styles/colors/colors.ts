480eb085f062db9e9ad8fc5056de6fed930b7581
patrache <ssk5442@naver.com>
2024-01-03 19:31:29 +0900

    [feat]: color, font style added

---
+++ public/styles/colors/colors.ts
@@ -0,0 +1,11 @@
+export enum Colors {
+    white = '#FDFDFD',
+    black = '#484848',
+    gray = '#CCCCCC',
+    lightGray = '#E5E5E5',
+    pink = '#FF8CAB',
+    yellow = '#FFEBAF',
+    pinkTransparent = '#FF8CAB66',
+    yellowTransparent = '#FFEBAF66',
+    transparent = 'transparent',
+}
