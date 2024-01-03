b9c3404ce5bc0e510b081d328061c0695c8cce47
patrache <ssk5442@naver.com>
2024-01-04 01:06:45 +0900

    [feat]: profile component added

---
+++ src/components/common/profile.tsx
@@ -0,0 +1,44 @@
+import styles from "./css/profile.module.css";
+import Image from "next/image";
+import fonts from "../../../public/styles/fonts/fonts.module.css";
+import { Colors } from "../../../public/styles/colors/colors";
+
+interface ProfileProps {
+  size?: "large" | "small";
+  imgUrl?: string;
+  username: string;
+  onClick?: () => void;
+}
+
+export const Profile = ({
+  size = "large",
+  imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7chlFoormeaJwlRP9IDIeTFw6ICl-rj3vA&usqp=CAU",
+  username = "unknown",
+  ...props
+}: ProfileProps) => {
+  const sizePixel = size === "large" ? 50 : 40;
+
+  return (
+    <div
+      className={[styles.profileContainer, styles["profile--" + size]].join(
+        " "
+      )}
+      {...props}
+    >
+      <Image
+        className={[styles.profileImage].join(" ")}
+        alt="profile image"
+        src={imgUrl}
+        width={sizePixel}
+        height={sizePixel}
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className={[styles.profileName, fonts.nsrBold].join(" ")}
+        style={{ color: Colors.black }}
+      >
+        {username}
+      </div>
+    </div>
+  );
+};
