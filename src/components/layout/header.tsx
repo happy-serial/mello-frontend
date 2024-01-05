76bd6faf85d6a8e34da19f03f6e67b1687cf781d
patrache <ssk5442@naver.com>
2024-01-06 00:29:55 +0900

    [feat]: header component added

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 import styles from "./css/header.module.css";
-import fonts from "../../../public/styles/fonts/fonts.module.css";
+import { blackOpsOne } from "../../../public/styles/fonts/fonts";
 import { Colors } from "../../../public/styles/colors/colors";
 
 import { Button } from "../common/button";
@@ -18,25 +18,30 @@ export const Header = ({ isLogin, username }: HeaderProps) => {
       style={{ borderBottom: "1px solid " + Colors.lightGray }}
     >
       <div className={[styles.headerContents].join(" ")}>
-        <Image
-          alt="company logo"
-          src="/resources/logo.png"
-          width={50}
-          height={50}
-          style={{ objectFit: "contain" }}
-          priority
-        />
+        <div className={styles.logo}>
+          <Image
+            alt="company logo"
+            src="/resources/logo.png"
+            width={60}
+            height={60}
+            style={{ objectFit: "contain" }}
+            priority
+          />
+          <div className={[styles.logoText, blackOpsOne.className].join(" ")}>
+            mello
+          </div>
+        </div>
         <div>
           <Button
-            label="로그인"
-            backgroundColor={Colors.transparent}
-            color={Colors.pink}
-            borderColor={Colors.pink}
-          />
-          <Button
-            label="회원가입"
+            label="JOIN"
             backgroundColor={Colors.transparent}
             color={Colors.black}
+          />
+          <Button
+            label="LOG IN"
+            backgroundColor={Colors.transparent}
+            color={Colors.black}
+            borderColor={Colors.black}
           />
         </div>
       </div>
