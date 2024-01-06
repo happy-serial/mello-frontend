c8c9009a925d92f1a253dd6e0f876ab3b36eb7c0
Kim Sangsu <39954425+patrache@users.noreply.github.com>
2024-01-07 02:16:55 +0900

    [feat]: auth background added (#7)
    Co-authored-by: chc216 <mine3386@naver.com>

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import { TextField } from "@/components/common/textField";
 import { blackOpsOne } from "../../../../public/styles/fonts/fonts";
 import { Colors } from "../../../../public/styles/colors/colors";
 import { LoginForm } from "@/components/auth/loginForm";
+import {Background} from "@/components/background"
 
 export default function Home() {
   return (
@@ -13,10 +14,10 @@ export default function Home() {
         height: "100vh",
         display: "flex",
         alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: Colors.black,
+        justifyContent: "center"
       }}
     >
+      <Background />
       <LoginForm/>
     </div>
   );
