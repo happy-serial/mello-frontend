8eb56e7e0de8cc66a762fb6edd18b3416b215871
patrache <ssk5442@naver.com>
2024-01-12 23:32:26 +0900

    [feat]: save token at social login

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,14 +2,15 @@ import type { Metadata } from "next";
 import { Header } from "../../components/layout/header";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "mello",
   description: "mello company",
   icons: {
-    icon: "/resources/logo.png",
+    icon: "/Image/logo.png",
   },
 };
+
+
 
 export default function RootLayout({
   children,
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,92 @@
+"server action";
+
 import NextAuth from "next-auth";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
 import GoogleProvider from "next-auth/providers/google";
 import { socialLogin } from "@/api";
 import { SocialLoginRequest } from "@/model";
+// import Cookies from "js-cookie";
+import { cookies } from "next/headers";
 
 const handler = NextAuth({
   providers: [
     NaverProvider({
-      async profile(profile) {
-        const requestData: SocialLoginRequest = {
-          id: profile.id,
-          provider: "NAVER",
-          email: profile.email,
-          name: profile.name,
-        };
-        const response = await socialLogin(requestData);
-        return {
-          id: profile.response.id,
-        };
-      },
       clientId: process.env.NAVER_CLIENT_ID || "",
       clientSecret: process.env.NAVER_CLIENT_SECRET || "",
     }),
     KakaoProvider({
-      async profile(profile) {
-        const requestData: SocialLoginRequest = {
-          id: profile.id,
-          provider: "KAKAO",
-          email: profile.kakao_account.email,
-          name: profile.kakao_account.nickname,
-        };
-        const response = await socialLogin(requestData);
-        return {
-          id: profile.response.id,
-        };
-      },
       clientId: process.env.KAKAO_CLIENT_ID || " ",
       clientSecret: process.env.KAKAO_CLIENT_SECRET || " ",
     }),
     GoogleProvider({
-      async profile(profile) {
-        const requestData: SocialLoginRequest = {
-          id: profile.sub,
-          provider: "GOOGLE",
-          email: profile.email,
-          name: profile.name,
-        };
-        const response = await socialLogin(requestData);
-        return {
-          id: profile.sub,
-        };
-      },
       clientId: process.env.GOOGLE_CLIENT_ID || " ",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || " ",
     }),
   ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account) {
+        delete account.accessToken;
+        delete account.refreshToken;
+      }
+      return token;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      let requestData;
+      const cookieStore = cookies();
+
+      if (account && profile) {
+        console.log(JSON.stringify(profile));
+        switch (account.provider) {
+          case "naver":
+            requestData = {
+              // @ts-ignore
+              id: profile.response.id,
+              provider: "NAVER",
+              // @ts-ignore
+              email: profile.response.email,
+              // @ts-ignore
+              name: profile.response.name,
+            };
+            break;
+          case "kakao":
+            requestData = {
+              // @ts-ignore
+              id: profile.id,
+              provider: "KAKAO",
+              // @ts-ignore
+              email: profile.kakao_account.email,
+              // @ts-ignore
+              name: profile.kakao_account.profile.nickname,
+            };
+            break;
+          case "google":
+            requestData = {
+              id: profile.sub!,
+              provider: "GOOGLE",
+              email: profile.email!,
+              name: profile.name!,
+            };
+            break;
+          default:
+        }
+        console.log(JSON.stringify(requestData));
+        try {
+          const response = await socialLogin(requestData!);
+          if (response) {
+            cookieStore.set("access-token", response.accessToken);
+            cookieStore.set("refresh-token", response.refreshToken);
+            return true;
+          }
+        } catch (error) {
+          return false;
+        }
+      }
+
+      return false;
+    },
+  },
   secret: "DLrZrrwH8v4QgF/3n0Nd28qAW9i3pO5Mlg==",
 });
 
