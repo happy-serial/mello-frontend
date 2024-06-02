8e04bd9b51fd4c35e5066bc281c6bb9892eb2ce7
patrache <ssk5442@naver.com>
2024-06-03 05:04:56 +0900

    [api]: create blog api added

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -1,4 +1,4 @@
-import { ImageURLRequest } from "@/model";
+import { ImageURLRequest, saveBlogRequest, tempBlogIdResponse, tempBlogRequest } from "@/model";
 import Cookies from "js-cookie";
 import { DefaultResponse, serverUrl } from ".";
 
@@ -36,10 +36,8 @@ export const getImageURL = async (file: File) => {
   }
 };
 
-export const summarizeText = async (
-  text: string,
-) => {
-  const accessToken = Cookies.get("access-token")
+export const summarizeText = async (text: string) => {
+  const accessToken = Cookies.get("access-token");
   console.log("summarizeText called");
   try {
     const response = await fetch(`${serverUrl}/gpt/convert-to-markdown`, {
@@ -61,5 +59,125 @@ export const summarizeText = async (
     return response.body!.getReader();
   } catch (error) {
     console.error("Failed to fetch data:", error);
+  }
+};
+
+export const saveImage = async (imageFile: File) => {
+  const accessToken = Cookies.get("access-token");
+  const formdata = new FormData();
+
+  formdata.append("file", imageFile);
+
+  try {
+    const response = await fetch(`${serverUrl}/image`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: formdata,
+    });
+    if (!response.ok && response.body !== null) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData: DefaultResponse<{ imageUrl: string }> =
+      await response.json();
+
+    if (responseData.statusCode === 201) {
+      return responseData.data?.imageUrl;
+    } else {
+      throw new Error(`Failed to save image: ${JSON.stringify(responseData)}`);
+    }
+  } catch (error) {
+    console.error("Failed to save image:", error);
+  }
+};
+
+// TODO: 응답 201
+const createTempBlog = async () => {
+  const accessToken = Cookies.get("access-token");
+  try {
+    const response = await fetch(`${serverUrl}/blog/temp`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok && response.body !== null) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData: DefaultResponse<tempBlogIdResponse> =
+      await response.json();
+
+    if (responseData.statusCode === 201) {
+      return responseData.data?.tempBlogId;
+    } else {
+      throw new Error(
+        `Failed to create temp blog: ${JSON.stringify(responseData)}`
+      );
+    }
+  } catch (error) {
+    console.error("Failed to create temp blog:", error);
+  }
+};
+
+// TODO: 응답 200
+const saveTempBlog = async (tempBlogInfo: tempBlogRequest) => {
+  const accessToken = Cookies.get("access-token");
+  try {
+    const response = await fetch(`${serverUrl}/blog/temp`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(tempBlogInfo)
+    });
+    if (!response.ok && response.body !== null) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData: DefaultResponse<Object> =
+      await response.json();
+
+    if (responseData.statusCode === 200) {
+      return "success";
+    } else {
+      throw new Error(
+        `Failed to save temp blog: ${JSON.stringify(responseData)}`
+      );
+    }
+  } catch (error) {
+    console.error("Failed to save temp blog:", error);
+  }
+};
+
+// TODO: 응답 201
+const saveBlog = async (blogInfo: saveBlogRequest) => {
+  const accessToken = Cookies.get("access-token");
+  try {
+    const response = await fetch(`${serverUrl}/blog`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(blogInfo)
+    });
+    if (!response.ok && response.body !== null) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData: DefaultResponse<Object> =
+      await response.json();
+
+    if (responseData.statusCode === 201) {
+      return "success";
+    } else {
+      throw new Error(
+        `Failed to save blog: ${JSON.stringify(responseData)}`
+      );
+    }
+  } catch (error) {
+    console.error("Failed to save blog:", error);
   }
 };
--- src/model/blog.model.ts
+++ src/model/blog.model.ts
@@ -23,3 +23,24 @@ export type createTemporaryBlogResponse = {
 export type ImageURLRequest = {
   imageUrl: string;
 };
+
+export type tempBlogIdResponse = {
+  tempBlogId: string;
+  createdAt: string;
+};
+
+export type tempBlogRequest = {
+  tempBlogId: string;
+  title: string;
+  category: string[];
+  contents: string;
+  classification: "DESIGN" | "TECH" | "CAREER" | "ETC";
+};
+
+export type saveBlogRequest = {
+  tempBlodId: "8dba3e2a-dbe5-4498-8700-a872e11ed794";
+  thumbnailUrl: "https://mello-s3-dev.s3.ap-northeast-1.amazonaws.com/image/1c9ac78d-1dfd-4870-81e4-11ae76921cf1";
+  accessStatus: "PUBLIC";
+  about: "string";
+  classification: "ETC"; // 대분류이다.
+};
